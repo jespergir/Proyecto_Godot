15627044371576a7299de098acf9617b1aaa94ec
jesus <jespergir95@gmail.com>
2025-05-20 12:03:43 +0200

    Añade sonido a los cristales

--- Recogibles/cristal.gd
+++ Recogibles/cristal.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 @onready var sprite : Sprite2D = $Sprite2D
 @onready var collision_shape : CollisionShape2D = $Area2D/CollisionShape2D
-
+@onready var audio = $AudioStreamPlayer
 
 func _on_area2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Protagonista"):
@@ -11,6 +11,7 @@ func _on_area2d_body_entered(body: Node2D) -> void:
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if animated_sprite.animation == "Disapear":
+		await audio.finished
 		call_deferred("queue_free") #Hacer desaparecer el item tras cogerlo.
 
 
@@ -18,4 +19,5 @@ func recoger():
 	collision_shape.set_deferred("disabled", true)
 	sprite.visible = false
 	animated_sprite.visible = true
+	audio.play()
 	animated_sprite.play("Disapear")
